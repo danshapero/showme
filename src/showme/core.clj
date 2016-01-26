b766b0f80d67b25c65e4a327bbe3a309c8bca069
danshapero <shapero.daniel@gmail.com>
2016-01-25 22:45:19 -0800

    Switched to quil fun mode

--- src/showme/core.clj
+++ src/showme/core.clj
@@ -1,5 +1,6 @@
 (ns showme.core
   (:require [quil.core :as q]
+            [quil.middleware :as m]
             [showme.mesh :as mesh])
   (:import java.awt.event.KeyEvent))
 
@@ -10,7 +11,7 @@
     [(* (/ (- x xmin) diam) width)
      (* (- 1.0 (/ (- y ymin) diam)) height)]))
 
-(defn draw []
+(defn draw [_]
   (q/background 255)
   (let [{:keys [x y edges]} (mesh/read-mesh @filename)]
     (let [xmin (apply min x)
@@ -23,13 +24,13 @@
             (q/line (f [(get x i) (get y i)])
                     (f [(get x j) (get y j)]))))))))
 
-(defn key-press []
-  (let [raw-key (q/raw-key)]
-    (when (= KeyEvent/VK_ESCAPE raw-key)
-      (q/exit))))
+(defn key-press [_ event]
+  (when (= KeyEvent/VK_ESCAPE (:raw-key event))
+    (q/exit)))
 
 (q/defsketch draw-mesh
   :size [300 300]
+  :middleware [m/fun-mode]
   :draw draw
   :key-pressed key-press)
 
